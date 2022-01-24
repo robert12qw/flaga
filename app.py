82eb95e6a4a53a7bfd919341e3df6e0ac3fcad36
robert12qw <robert.rad@interia.pl>
2022-01-24 21:28:57 +0100

    zaktualizowalem plik

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    text = 'czesc tu kasia'
+    text = 'czesc tu robert'
     return render_template("index.html", text=text)
 
 if __name__=="__main__":
