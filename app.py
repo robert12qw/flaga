770b34c793d225c6c9d49e44d564efc60bffdb50
robert12qw <robert.rad@interia.pl>
2022-02-16 17:47:08 +0100

    jw

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    text = open('dane/xd.txt').read()
+    text = open('xd.txt').read()
     return render_template("index.html", text=text)
 
 @app.route('/xd')
