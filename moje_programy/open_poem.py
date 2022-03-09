f986e4ac8336c16758ab332c7059e734e557f9bd
robert <leski.robert@gmail.com>
2022-03-09 21:49:15 +0200

    test

---
+++ moje_programy/open_poem.py
@@ -0,0 +1,9 @@
+    def open_poem():
+    text_lines = open('dane/poem.txt').readlines()
+
+    poem_lines = []
+    for line in text_lines:
+        line = line.strip()
+        poem_lines.append(line)
+        
+    return poem_lines
