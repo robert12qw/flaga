f305dba63c01115802f8df423e055b2dff57f9c5
robert <leski.robert@gmail.com>
2022-03-14 20:17:58 +0200

    sw

---
+++ moje_programy/1.py
@@ -0,0 +1,14 @@
+zmienna = -4 # 150
+if zmienna == 1:
+    print('Warunek 1 spełniony')
+elif:
+    print('Warunek 1 nie spełniony, więc realizuje się kod po "else"')
+
+    #we wszystkich plikach pozmieniałem słowa,wszystko trzeba poprawić
+
+    if przepis = chalka:
+    open
+
+    if
+
+    if
