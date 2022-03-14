7642334613d1cdedf8d6bae9e36b53bb18929342
robert <leski.robert@gmail.com>
2022-03-14 20:26:27 +0200

    postacie

---
+++ moje_programy/open_przepis.py
@@ -0,0 +1,19 @@
+if jezeli wybrałeś coś to cos
+jeżeli budyń towyfrukuj 
+
+
+def character( name):
+    content = wiki.summary(name, sentences=6)
+    return content
+
+
+def open_przepis( przepis):
+if przepis == budyn
+    text_lines = open('dane/budyn.txt', encoding='utf-8').readlines()
+
+    budyń_lines = []
+    for line in text_lines:
+        line = line.strip()
+        budyń_lines.append(line)
+    
+    return budyń_lines
